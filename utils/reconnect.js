33d5a8f6f120dfbfefc797641cc6c7ff1db6d9db
Zed <skhull.coding@proton.me>
2025-02-01 11:41:07 +0530

    async reconnect

--- utils/reconnect.js
+++ utils/reconnect.js
@@ -3,14 +3,17 @@
  * @param {[Function]} func
  * @param {[Any]} stopVal return value of function at which to not retry
  * @param {[Integer]} interval time in miliseconds
+ * @return {Promise<void>}
  */
 const retry = async (func, stopVal = true, interval = 3000) => {
   const functionResult = await func();
   if (functionResult !== stopVal)
-    setTimeout(() => {
-      const functionResult = func();
-      if (functionResult !== stopVal) retry(func, stopVal, interval);
-    }, interval);
+    await new Promise(() => {
+      setTimeout(() => {
+        const functionResult = func();
+        if (functionResult !== stopVal) retry(func, stopVal, interval);
+      }, interval);
+    });
 };
 
 export default retry;
