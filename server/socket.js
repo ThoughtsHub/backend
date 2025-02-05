51d0488a998f3632f28071065e308c73a191e2d1
Zed <skhull.coding@proton.me>
2025-02-05 20:33:46 +0530

    feat: socket on join and leave

--- server/socket.js
+++ server/socket.js
@@ -5,5 +5,13 @@ import { _app } from "./main.js";
 const server = createServer(_app); // http server
 const io = new Server(server); // socket server
 
+io.on("connection", (socket) => {
+  console.log("New Client Connected.");
+
+  socket.on("disconnect", () => {
+    console.log("Client Disconnected.");
+  });
+});
+
 export const _io = io;
 export default server;
