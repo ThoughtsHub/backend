c567afd26da8d601d56c2e9be8b66ce6716d9341
Zed <skhull.coding@proton.me>
2025-02-07 09:34:28 +0530

    fix: vote associations linked

--- associations/association.js
+++ associations/association.js
@@ -4,6 +4,7 @@ import forumAssociations from "./forum.js";
 import profileAssociations from "./profile.js";
 import uploadAssociations from "./upload.js";
 import userAssociations from "./user.js";
+import voteAssociations from "./vote.js";
 
 const initAssociation = () => {
   userAssociations();
@@ -11,6 +12,7 @@ const initAssociation = () => {
   profileAssociations();
   educationAssociations();
   forumAssociations();
+  voteAssociations();
 
   db.sync({ alter: true }); // after linking, sync
 };
