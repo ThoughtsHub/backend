aea1404b09bf7389338bef762acd7f6a48a2bc39
Zed <skhull.coding@proton.me>
2025-02-15 23:31:29 +0530

    feat: forums update on profile

--- associations/forum.js
+++ associations/forum.js
@@ -1,6 +1,8 @@
 import Comment from "../models/Comment.js";
 import Forum from "../models/Forum.js";
+import Profile from "../models/Profile.js";
 import Vote from "../models/Vote.js";
+import dbUnary from "../utils/db.js";
 
 const F_KEY = "forumId";
 
@@ -10,6 +12,15 @@ const forumAssociation = () => {
 
   Forum.hasMany(Vote, { foreignKey: F_KEY, onDelete: "CASCADE" });
   Vote.belongsTo(Forum, { foreignKey: F_KEY });
+
+  Forum.afterCreate(async (payload, options) => {
+    const t = options.transaction;
+    await dbUnary.increment1(Profile, "forums", payload.forumId, t);
+  });
+  Forum.afterDestroy(async (payload, options) => {
+    const t = options.transaction;
+    await dbUnary.decrement1(Profile, "forums", payload.forumId, t);
+  });
 };
 
 export default forumAssociation;
