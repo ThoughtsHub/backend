5fedc75be2a27972bde2d5f888123db8f05bfe57
Zed <skhull.coding@proton.me>
2025-02-24 23:48:57 +0530

    feat: news can be created in bulk

--- routes/news.js
+++ routes/news.js
@@ -41,19 +41,44 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", auth.login, auth.admin, async (req, res) => {
-  const { title, description, image, images, tags, category, newsUrl } =
-    _req.getDataO(req.body, NEWS_FIELDS);
+  const { title, description, image, images, tags, category, newsUrl, news } =
+    _req.getDataO(req.body, [...NEWS_FIELDS, "news"]);
 
-  if (typeof image === "string") images = [image];
+  const _news = [];
+  if (Array.isArray(news)) {
+    for (const n of news) {
+      const newsData = _req.getDataO(n, NEWS_FIELDS);
 
-  if (!Array.isArray(images)) return res.bad("No image given");
+      if (typeof newsData.image === "string") newsData.images = [image];
+      delete newsData.image;
 
-  if (_req.anyNull(title, description, images, newsUrl)) return res.noParams();
+      if (!Array.isArray(newsData.images)) return res.bad("No image given");
 
-  const newsHandle = handle.create(title);
+      if (
+        _req.anyNull(
+          newsData.title,
+          newsData.description,
+          newsData.images,
+          newsData.newsUrl
+        )
+      )
+        return res.noParams();
 
-  try {
-    const news = await News.create({
+      newsData.handle = handle.create(newsData.title);
+
+      _news.push(newsData);
+    }
+  } else {
+    if (typeof image === "string") images = [image];
+
+    if (!Array.isArray(images)) return res.bad("No image given");
+
+    if (_req.anyNull(title, description, images, newsUrl))
+      return res.noParams();
+
+    const newsHandle = handle.create(title);
+
+    _news.push({
       title,
       description,
       images,
@@ -62,8 +87,12 @@ router.post("/", auth.login, auth.admin, async (req, res) => {
       newsUrl,
       handle: newsHandle,
     });
+  }
 
-    res.created("News Created", { news });
+  try {
+    const news = await News.bulkCreate(_news);
+
+    res.created("News Created", { news: _news });
   } catch (err) {
     console.log(err);
 
