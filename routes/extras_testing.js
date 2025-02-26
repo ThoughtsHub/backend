d92873e2a975b2aa37e726110c483e58aab45a58
Zed <skhull.coding@proton.me>
2025-02-26 14:00:58 +0530

    feat: testing purposes endpoints

---
+++ routes/extras_testing.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import auth from "../middlewares/auth.js";
+import { spawn } from "child_process";
+import env from "../constants/env.js";
+
+const isWindows = process.platform === "win32";
+
+// Path to reload shell script
+const scriptPath = `./scripts/reload.${isWindows ? "ps1" : "sh"}`;
+const reloadProgram = isWindows ? "powershell.exe" : "bash";
+const reloadOptions = isWindows ? ["-File", scriptPath] : [scriptPath];
+
+const router = Router();
+
+router.get("/test", auth.login, (req, res) => {
+  res.json({ user: req.user });
+});
+
+router.get("/reload-website", async (_, res) => {
+  const child = spawn(reloadProgram, reloadOptions, {
+    env: { ...env.app },
+    detached: true,
+    stdio: "ignore",
+    shell: isWindows,
+  });
+
+  // detach process
+  child.unref();
+
+  res.send("Restarting....");
+  process.exit(0);
+});
+
+export const TestRouter = router;
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,4 @@
 import { Router } from "express";
-import { spawn } from "child_process";
-import { APP } from "../constants/env.js";
 import auth from "../middlewares/auth.js";
 import { LoginRouter } from "./login.js";
 import { LogoutRouter } from "./logout.js";
@@ -9,9 +7,7 @@ import { ProfileRouter } from "./profile.js";
 import { SchoolRouter } from "./school.js";
 import { NewsRouter } from "./news.js";
 import { SignupRouter } from "./signup.js";
-
-// Path to your shell script
-const reloadScriptPath = "./scripts/reload.sh";
+import { TestRouter } from "./extras_testing.js";
 
 const app = Router();
 
@@ -25,21 +21,7 @@ app.use("/profile", ProfileRouter);
 app.use("/school", SchoolRouter);
 app.use("/news", NewsRouter);
 
-app.get("/test", auth.login, (req, res) => {
-  res.json({ user: req.user });
-});
-
-app.get("/reload-website", async (_, res) => {
-  const child = spawn("bash", [reloadScriptPath], {
-    detached: true,
-    stdio: "ignore",
-  });
-
-  // detach process
-  child.unref();
-
-  res.send("Restarting....");
-  process.exit(0);
-});
+// for testing purposes
+app.use("/", TestRouter);
 
 export const appRouter = app;
