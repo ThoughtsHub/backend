ec07efc2891caae3eade3ae1557863910c8acbb8
Zed <skhull.coding@proton.me>
2025-02-27 10:10:24 +0530

    docs: comments for better understanding

--- controllers/otp.js
+++ controllers/otp.js
@@ -24,16 +24,19 @@ const getOtp = async (req, res) => {
   const confirmationId = `${key}:${v4()}`;
 
   try {
-    await client.del(`otp:${key}`);
+    await client.del(`otp:${key}`); // delete the prev otp
 
+    // set a new otp for the same email/otp
     await client.setEx(`otp:${key}`, 5 * 60, generatedOtp);
-    await client.setEx(confirmationId, 5 * 60, key);
+    await client.setEx(confirmationId, 5 * 60, key); // and confirmationId
 
+    // send otp based on if mobile or email given
     const sendOtp = isMobile ? otp.sendMobile : otp.sendEmail;
     const otpSent = await sendOtp(key, generatedOtp);
 
     if (otpSent) return res.ok("OTP sent", { otpSent, confirmationId });
 
+    // delete the generated otp and confirmationId if otp not sent
     await client.del([`otp:${key}`, confirmationId]);
 
     res.bad("OTP could not be sent", { otpSent });
@@ -57,16 +60,18 @@ const verifyOtp = async (req, res) => {
 
   const [otp, confirmationId] = body.bulkGet("otp confirmationId");
   try {
-    const key = await client.get(confirmationId);
+    const key = await client.get(confirmationId); // check the confirmationId
     if (key === null) return res.bad("Invalid confirmation Id");
 
+    // the otp that was set for that email/mobile
     const setOtp = await client.get(`otp:${key}`);
     if (setOtp !== otp)
       return res.bad("Invalid OTP", { confirmationCodeMatched: false });
 
+    // create a new confirmationId for setting a password
     const newConfirmationId = `password:${key}:${v4()}`;
     await client.setEx(newConfirmationId, 5 * 60, key);
-    await client.del([`otp:${key}`, confirmationId]);
+    await client.del([`otp:${key}`, confirmationId]); // delete the verified otp
 
     res.ok("OTP verified", {
       confirmationCodeMatched: true,
