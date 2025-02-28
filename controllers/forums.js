c3262c3a54b8ce8cc96bbcc89170df0a39099e04
Zed <skhull.coding@proton.me>
2025-02-28 17:15:10 +0530

    feat: forums of users and current user

--- controllers/forums.js
+++ controllers/forums.js
@@ -34,4 +34,62 @@ const getForums = async (req, res) => {
   }
 };
 
-export const ForumController = { getForums };
+const getMyForums = async (req, res) => {
+  const profileId = req.user.profile.id;
+
+  const query = new ReqBody(req.query);
+  query.toNumber("offset", 0);
+
+  const offset = query.get("offset");
+  try {
+    const forums = await Forum.findAll({
+      where: { profileId },
+      offset,
+      limit: 30,
+      order: [["createdAt", "desc"]],
+      include: [
+        { model: Comment, offset: 0, limit: 2, order: [["createdAt", "desc"]] },
+        { model: Vote, offset: 0, limit: 4, order: [["createdAt", "desc"]] },
+      ],
+    });
+
+    res.ok("Forums", { forums });
+  } catch (err) {
+    console.log(err);
+
+    res.serverError();
+  }
+};
+
+const getSomeoneForums = async (req, res) => {
+  const query = new ReqBody(req.query);
+  query.toNumber("offset", 0);
+
+  if (!query.isString("profileId")) return res.bad("Invalid profileId");
+
+  const offset = query.get("offset");
+  const profileId = query.get("profileId");
+  try {
+    const profile = await Profile.findByPk(profileId);
+    if (profile === null) return res.bad("Invalid profileId");
+
+    const forums = await Forum.findAll({
+      where: { profileId },
+      offset,
+      limit: 30,
+      order: [["createdAt", "desc"]],
+      include: [
+        { model: Comment, offset: 0, limit: 2, order: [["createdAt", "desc"]] },
+        { model: Vote, offset: 0, limit: 4, order: [["createdAt", "desc"]] },
+      ],
+    });
+
+    res.ok("Forums", { forums });
+  } catch (err) {
+    console.log(err);
+
+    res.serverError();
+  }
+};
+
+export const ForumController = { getForums, getMyForums, getSomeoneForums };
--- routes/forums.js
+++ routes/forums.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import controller from "../controllers/controllers.js";
+import auth from "../middlewares/auth.js";
 
 const ForumController = controller.forums;
 
 const router = Router();
 
 router.get("/", ForumController.getForums);
+
+router.get("/me", auth.login, auth.profile, ForumController.getMyForums);
+
+router.get("/of", ForumController.getSomeoneForums);
 
 export const ForumsRouter = router;
