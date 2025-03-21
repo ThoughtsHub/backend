c4713fe7a62c2b88fe4fdcf683e6158fe8c644e6
Zed <skhull.coding@proton.me>
2025-03-21 18:46:37 +0530

    chore: associations happen after connection

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import initAssociations from "./associations/associations.js";
 import { connectToPg } from "./db/pg.js";
 import { connectToRedis } from "./db/redis.js";
 import env from "./env/env.config.js";
@@ -6,6 +7,7 @@ import shutdown from "./utils/shutdown.js";
 
 await connectToRedis();
 await connectToPg();
+await initAssociations();
 
 server.listen(env.server.port, env.server.host, () => {
   console.log(`Server started on http://${env.server.host}:${env.server.port}`);
