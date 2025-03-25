c0606d08fe25c4061498b68a91707df87976026b
Zed <skhull.coding@proton.me>
2025-03-25 21:27:18 +0530

    fix: username -> userId

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -2,35 +2,35 @@ import client from "../../db/redis.js";
 import { hashSHA256 } from "../../utils/hash.js";
 import { v4 as uuidv4 } from "uuid";
 
-export const setupAuth = async (username) => {
+export const setupAuth = async (userId) => {
   const nowTime = Date.now().toString();
-  const usernameHash = hashSHA256(username);
+  const userIdHash = hashSHA256(userId);
   const randomUniqueString = uuidv4();
 
   // unique user token
-  const userToken = `${nowTime}-${randomUniqueString}-${usernameHash}`;
+  const userToken = `${nowTime}-${randomUniqueString}-${userIdHash}`;
 
   // expiry of 5 days
-  await client.setEx(userToken, 5 * 24 * 60 * 60, username); // set userToken to username
+  await client.setEx(userToken, 5 * 24 * 60 * 60, userId); // set userToken to userId
 
   return userToken;
 };
 
 export const validateAuth = async (userToken) => {
-  const username = await client.get(userToken);
+  const userId = await client.get(userToken);
 
-  const result = { valid: username !== null, username };
+  const result = { valid: userId !== null, userId };
   return result;
 };
 
 export const auth = async (req, _, next) => {
   const userToken = req.headers["authorization"]?.split(" ")?.[1] ?? "null";
 
-  const { valid, username } = await validateAuth(userToken);
+  const { valid, userId } = await validateAuth(userToken);
 
   if (valid) {
     req.loggedIn = true;
-    req.username = username;
+    req.userId = userId;
     req.userToken = userToken;
   } else req.loggedIn = false;
 
@@ -43,7 +43,7 @@ export const loggedIn = (req, res, next) => {
 };
 
 export const loggedAsAdmin = (req, res, next) => {
-  if (req.loggedIn && req.username === "admin") next();
+  if (req.loggedIn && req.userId === "admin") next();
   else res.forbidden("Only admins are allowed");
 };
 
--- routes/login.js
+++ routes/login.js
@@ -20,7 +20,7 @@ router.post("/login", async (req, res) => {
     if (user === null) return res.failure(`Bad ${givenField}`);
     if (user.password !== password) return res.unauth("Wrong password");
 
-    const userToken = await setupAuth(user[givenField]);
+    const userToken = await setupAuth(user.id);
     res.ok("Log in successful", { userToken });
   } catch (err) {
     console.log(err);
@@ -61,7 +61,7 @@ router.post("/signup/create-password", async (req, res) => {
 
     const user = await User.create({ [givenField]: contact, password });
 
-    const userToken = await setupAuth(contact);
+    const userToken = await setupAuth(user.id);
     res.created("Sign up successful", { userToken });
   } catch (err) {
     console.log(err);
