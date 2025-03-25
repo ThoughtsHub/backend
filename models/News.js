bb852616858e702ee9a1f2fd1e4ab475350ee157
Zed <skhull.coding@proton.me>
2025-03-25 20:53:56 +0530

    feat: news model

---
+++ models/News.js
@@ -0,0 +1,19 @@
+import { types } from "../constants/db.js";
+import { hooks, timestamps } from "../constants/timestamps.js";
+import db from "../db/pg.js";
+
+const News = db.define(
+  "News",
+  {
+    id: types.ID,
+    title: types.STR_REQ,
+    body: types.TEXT_REQ,
+    image: types.STRING,
+    newsUrl: types.STRING,
+    category: types.STR_REQ,
+    ...timestamps,
+  },
+  { hooks, tableName: "News" }
+);
+
+export default News;
