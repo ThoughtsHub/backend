9316cf3b4a95da4225d851136d12ef4c8c833f0a
Zed <skhull.coding@proton.me>
2025-03-27 17:23:37 +0530

    chore: associate story models with its couterparts

--- associations/associations.js
+++ associations/associations.js
@@ -1,12 +1,14 @@
 import db from "../db/pg.js";
 import forumAssociations from "./forums.js";
 import profileAssociations from "./profile.js";
+import storyAssociations from "./story.js";
 import userAssociations from "./user.js";
 
 const initAssociations = async () => {
   userAssociations();
   profileAssociations();
   forumAssociations();
+  storyAssociations();
 
   db.sync({ alter: true });
 };
