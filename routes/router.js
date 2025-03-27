99421b06e241b846dc71ca15c1e559bce39bbe73
Zed <skhull.coding@proton.me>
2025-03-27 08:35:58 +0530

    chore: all types of endpoints

--- routes/router.js
+++ routes/router.js
@@ -5,14 +5,20 @@ import { TestRouter } from "./extras_testing.js";
 import { OtherRouter } from "./other.js";
 import { ProfileRouter } from "./profile.js";
 import { ProfileCollegeRouter } from "./school.js";
+import { ForumsRouter } from "./forums.js";
+import { StoryRouter } from "./story.js";
+import { NewsRouter } from "./news.js";
 
 const router = Router();
 
 router.use("/", LoginRouter);
-router.use("/otp", OtpRouter);
 router.use("/", TestRouter);
 router.use("/", OtherRouter);
+router.use("/otp", OtpRouter);
 router.use("/profile", ProfileRouter);
 router.use("/school", ProfileCollegeRouter);
+router.use("/forums", ForumsRouter);
+router.use("/story", StoryRouter);
+router.use("/news", NewsRouter);
 
 export const AppRouter = router;
