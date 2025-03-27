baaa99da43c8bc1734e009f12ccac61ca33a57a8
Zed <skhull.coding@proton.me>
2025-03-27 16:30:10 +0530

    feat: get news endpoint

--- routes/news.js
+++ routes/news.js
@@ -1,5 +1,40 @@
 import { Router } from "express";
+import News from "../models/News.js";
+import { Op } from "sequelize";
+import { timestampsKeys } from "../constants/timestamps.js";
 
 const router = Router();
+
+router.get("/", async (req, res) => {
+  const body = req.query;
+
+  const category = body.get("category", "all");
+  const timestamp = body.get("timestamp", null);
+  const whereObj = category === "all" ? {} : { category };
+
+  try {
+    let news;
+    if (body.isNumber("timestamp"))
+      news = await News.findAll({
+        where: {
+          ...whereObj,
+          [timestampsKeys.createdAt]: { [Op.gte]: timestamp },
+        },
+        limit: 30,
+        order: [[timestampsKeys.updatedAt, "DESC"]],
+      });
+    else
+      news = await News.findAll({
+        where: { ...whereObj },
+        limit: 30,
+        order: [[timestampsKeys.updatedAt, "DESC"]],
+      });
+
+    res.ok("News", { news });
+  } catch (err) {
+    console.log(err);
+    res.serverError();
+  }
+});
 
 export const NewsRouter = router;
