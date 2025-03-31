b65c7753014f8f23af141d79373b12fd45192f6b
Zed <skhull.coding@proton.me>
2025-03-31 11:11:23 +0530

    feat: logging module extended for many purposes

--- utils/logging.js
+++ utils/logging.js
@@ -1,18 +1,50 @@
 import fs from "fs";
 
 class Logger {
-  constructor(filename) {
-    this.filename = filename;
+  constructor(
+    logFilename,
+    errorFilename,
+    infoFilename,
+    warnFilename,
+    writeAllowed = true,
+    alternateFunction = console.log
+  ) {
+    this.logFilename = logFilename;
+    this.errorFilename = errorFilename;
+    this.infoFilename = infoFilename;
+    this.warnFilename = warnFilename;
+    this.writeAllowed = writeAllowed;
+    this.alternateFunction = alternateFunction;
   }
+
+  allowedToWrite = () => this.writeAllowed === true;
 
   log() {}
 
   error(err, message = "Error", user = null) {
-    const writeMessage = `${this.title(user)} : ${message}\n${err}\n---\n`;
-    this.write(writeMessage);
+    const writeMessage = `${this.title(user)} : ${message}\n${err}\n----\n`;
+    this.writeError(writeMessage);
   }
 
-  info() {}
+  info(message = "", user = null, variables = {}) {
+    let writeMessage = `${this.title(user)} : ${message}\n`;
+    for (const vars in variables) {
+      writeMessage += `${vars} : \n`;
+      writeMessage += `${variables[vars]}\n--\n`;
+    }
+    writeMessage += "----\n";
+    this.writeInfo(writeMessage);
+  }
+
+  warning(message = "", user = null, variables = {}) {
+    let writeMessage = `${this.title(user)} : ${message}\n`;
+    for (const vars in variables) {
+      writeMessage += `${vars} : \n`;
+      writeMessage += `${variables[vars]}\n--\n`;
+    }
+    writeMessage += "----\n";
+    this.writeWarning(writeMessage);
+  }
 
   title(user = "random") {
     return `[${new Date(Date.now()).toLocaleString()}] (${
@@ -20,8 +52,21 @@ class Logger {
     })`;
   }
 
-  write(message) {
-    fs.appendFileSync(this.filename, message);
+  writeError(message) {
+    this.write(this.errorFilename, message);
+  }
+
+  writeInfo(message) {
+    this.write(this.infoFilename, message);
+  }
+
+  writeWarning(message) {
+    this.write(this.warnFilename, message);
+  }
+
+  write(filename, message) {
+    if (this.allowedToWrite()) fs.appendFileSync(filename, message);
+    else this.alternateFunction(message);
   }
 }
 
