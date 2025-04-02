785279eb66b343acbf015288766f07f0cfaa7677
Zed <skhull.coding@proton.me>
2025-04-02 15:24:39 +0530

    fix: only when user is profiled

--- routes/login.js
+++ routes/login.js
@@ -50,9 +50,11 @@ router.post("/login", async (req, res) => {
 
     let profile = await Profile.findOne({ where: { userId: user.id } });
 
-    profile = profile.get({ plain: true });
-    profile.profileId = profile.id;
-    delete profile.id;
+    if (profile !== null) {
+      profile = profile.get({ plain: true });
+      profile.profileId = profile.id;
+      delete profile.id;
+    }
 
     const userToken = await setupAuth(user.id);
     res.ok("Log in successful", {
