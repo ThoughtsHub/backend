150ee217c75ab5a2f5e2c22f1a3b6454de93ab3e
Zed <skhull.coding@proton.me>
2025-05-25 12:44:58 +0530

    fix: update all news status to published for now

--- scripts/update.js
+++ scripts/update.js
@@ -3,29 +3,19 @@ import { connectToPg } from "../db/pg.js";
 import Forum from "../models/Forum.js";
 import ForumAppreciation from "../models/Forum_Appreciation.js";
 import ForumComment from "../models/Forum_Comment.js";
+import News from "../models/News.js";
 import { ForumComment_ } from "../services/ForumCommentService.js";
 import { Forum_ } from "../services/ForumService.js";
+import { News_ } from "../services/NewsService.js";
 
 await connectToPg();
 await initLink();
 
-const countAllLikes = async () => {
-  const forums = await Forum.findAll();
-  for (const forum of forums) {
-    const likes = await ForumAppreciation.count({
-      where: { forumId: forum.id },
-    });
-    const comments = await ForumComment.count({ where: { forumId: forum.id } });
-    try {
-      const result = await Forum.update(
-        { appreciations: likes, comments },
-        { where: { id: forum.id } }
-      );
-      console.log(result);
-    } catch (err) {
-      console.log(err);
-    }
+const updateNewsStatus = async () => {
+  const news = await News.findAll();
+  for (const n of news) {
+    let result = News_.update({ status: "Published" }, n.id);
   }
 };
 
-await countAllLikes();
+await updateNewsStatus()
