cdd7b0b122e540e703ba14f081174271d93f77d4
Zed <skhull.coding@proton.me>
2025-05-26 11:12:56 +0530

    fix: in reports, the forum info and the both writer info is sent

--- services/ReportService.js
+++ services/ReportService.js
@@ -184,6 +184,7 @@ class ForumReportService {
               include: [["id", "forumId"]],
               exclude: ["id"],
             },
+            include: [includeWriter],
           },
           includeWriter,
         ],
