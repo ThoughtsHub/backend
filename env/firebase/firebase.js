1175a47c6a7794bf5a07d81e1240155199281d0b
Zed <skhull.coding@proton.me>
2025-06-17 21:52:43 +0530

    fix: firebase config

--- env/firebase/firebase.js
+++ env/firebase/firebase.js
@@ -1,8 +1,13 @@
 import firebase from "firebase-admin";
 import { firebaseConfigFile } from "../env.config.js";
+import fs from "fs";
+
+const firebaseConfig = JSON.parse(
+  fs.readFileSync(firebaseConfigFile).toString()
+);
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(firebaseConfigFile),
+  credential: firebase.credential.cert(firebaseConfig),
 });
 
 export default firebase;
